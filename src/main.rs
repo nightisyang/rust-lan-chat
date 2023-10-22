1783c013c2737d3890cc4b9cfde8357810673700
nightisyang <shenyangc86@gmail.com>
2023-10-22 23:39:55 +0800

    added logging, fix no chat received, add cosmetic intro

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use chrono::Local;
 use clap::{App, Arg};
+use std::fs::OpenOptions;
 use std::io;
+use std::io::prelude::*;
 use std::io::Write;
 use std::net::{SocketAddr, UdpSocket};
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
-use std::thread;
+use std::{thread, time};
 
 fn main() -> io::Result<()> {
     // spawn a thread to listen for messages
@@ -23,9 +26,33 @@ fn main() -> io::Result<()> {
         // Add other arguments here in the future
         .get_matches();
 
-    let bind_addr = matches.value_of("bind").unwrap_or("0.0.0.0:8888").to_string();
+    let bind_addr = matches
+        .value_of("bind")
+        .unwrap_or("0.0.0.0:8888")
+        .to_string();
+
+    let intro_done = Arc::new(Mutex::new(false));
+    let intro_done_clone = Arc::clone(&intro_done);
+
+    let intro_thread = thread::spawn(move || {
+        let intro_lines = vec![
+            "Wake up, Neo...",
+            "The Matrix has you...",
+            "Follow the white rabbit.",
+            "Knock, knock, Neo.",
+        ];
+
+        for line in intro_lines {
+            print_intro_line(line);
+            thread::sleep(time::Duration::from_secs(1));
+        }
+
+        *intro_done_clone.lock().unwrap() = true;
+    });
+
     let bind_addr_clone = bind_addr.clone();
     let send_socket = UdpSocket::bind(&bind_addr_clone)?;
+    let socket_clone = send_socket.try_clone()?;
 
     let chat_history = Arc::new(Mutex::new(Vec::new()));
     let chat_history_recv_clone = Arc::clone(&chat_history);
@@ -39,7 +66,7 @@ fn main() -> io::Result<()> {
 
     // Receiver thread
     thread::spawn(move || {
-        let result = listen_for_message(&bind_addr, rx_tx);
+        let result = listen_for_message(&socket_clone, rx_tx);
         if let Err(e) = result {
             eprintln!("Error in listener thread: {}", e);
         }
@@ -74,6 +101,9 @@ fn main() -> io::Result<()> {
         }
     });
 
+    // Optionally, wait for the intro to finish before proceeding
+    intro_thread.join().expect("The intro thread panicked");
+
     clear_screen();
     print_prompt();
 
@@ -88,16 +118,29 @@ fn main() -> io::Result<()> {
         }
 
         if let Ok(message) = tx_rx.try_recv() {
-            chat_history_send_clone.lock().unwrap().push(message);
-            clear_screen();
-            print_chat(&chat_history_send_clone);
-            print_prompt();
+            // chat_history_send_clone.lock().unwrap().push(message);
+            // clear_screen();
+            // print_chat(&chat_history_send_clone);
+            // print_prompt();
         }
 
         // sleep for a short duration to avoid busy-waiting
         thread::sleep(std::time::Duration::from_millis(100));
     }
+
+
 }
+
+fn print_intro_line(line: &str) {
+    let chars: Vec<char> = line.chars().collect();
+    for ch in chars {
+        print!("{}", ch);
+        std::io::stdout().flush().unwrap();
+        thread::sleep(time::Duration::from_millis(50));
+    }
+    println!();  // Move to the next line after printing the current line
+}
+
 
 fn send_message(socket: &UdpSocket, send_msg: &str) -> io::Result<()> {
     let broadcast_address: SocketAddr = "255.255.255.255:8888".parse().unwrap();
@@ -108,16 +151,26 @@ fn send_message(socket: &UdpSocket, send_msg: &str) -> io::Result<()> {
     Ok(())
 }
 
-fn listen_for_message(addr: &str, tx: mpsc::Sender<String>) -> io::Result<()> {
-    let socket = UdpSocket::bind(addr)?; // listening on port 8888
-
+fn listen_for_message(socket: &UdpSocket, tx: mpsc::Sender<String>) -> io::Result<()> {
     let mut buffer = [0u8; 1024];
     loop {
         let (amt, src) = socket.recv_from(&mut buffer)?;
         let message = format!("{}: {}", src, String::from_utf8_lossy(&buffer[..amt]));
+        log_messages(&message)?;
         tx.send(message)
             .expect("Failed to send message to main thread");
     }
+}
+
+fn log_messages(message: &str) -> io::Result<()> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("chat_log.txt")?;
+
+    let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    writeln!(file, "[{}] {}", timestamp, message)
 }
 
 fn print_chat(chat_history: &Arc<Mutex<Vec<String>>>) {
