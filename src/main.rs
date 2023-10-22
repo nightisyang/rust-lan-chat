73f0ac2e276a03d98760145738ae0710fc5d3781
nightisyang <shenyangc86@gmail.com>
2023-10-22 22:34:11 +0800

    add mutex to chat history and use 8888 bind port

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::io;
 use std::io::Write;
 use std::net::{SocketAddr, UdpSocket};
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
-
 
 fn main() -> io::Result<()> {
     // spawn a thread to listen for messages
@@ -14,7 +14,7 @@ fn main() -> io::Result<()> {
         .about("Chat app for LAN")
         .arg(
             Arg::with_name("bind")
-            .short("b")
+                .short("b")
                 .long("bind")
                 .value_name("ADDRESS")
                 .help("Sets an address to bind to")
@@ -23,11 +23,13 @@ fn main() -> io::Result<()> {
         // Add other arguments here in the future
         .get_matches();
 
-    let bind_addr = matches.value_of("bind").unwrap_or("0.0.0.0:0").to_string();
+    let bind_addr = matches.value_of("bind").unwrap_or("0.0.0.0:8888").to_string();
     let bind_addr_clone = bind_addr.clone();
     let send_socket = UdpSocket::bind(&bind_addr_clone)?;
 
-    let mut chat_history: Vec<String> = Vec::new();
+    let chat_history = Arc::new(Mutex::new(Vec::new()));
+    let chat_history_recv_clone = Arc::clone(&chat_history);
+    let chat_history_send_clone = Arc::clone(&chat_history);
 
     // Receiver thread channel
     let (rx_tx, rx_rx) = mpsc::channel();
@@ -35,7 +37,6 @@ fn main() -> io::Result<()> {
     // Sender thread channel
     let (tx_tx, tx_rx) = mpsc::channel();
 
-    
     // Receiver thread
     thread::spawn(move || {
         let result = listen_for_message(&bind_addr, rx_tx);
@@ -48,7 +49,9 @@ fn main() -> io::Result<()> {
     thread::spawn(move || {
         loop {
             let mut input = String::new();
-            io::stdin().read_line(&mut input).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
             let input = input.trim();
 
             if input.is_empty() {
@@ -65,8 +68,9 @@ fn main() -> io::Result<()> {
             send_message(&send_socket, input).expect("Failed to send message");
 
             // send the user's messages to the main thread to update chat history
-            tx_tx.send(user_message).expect("Failed to send user message to main thread");
-
+            tx_tx
+                .send(user_message)
+                .expect("Failed to send user message to main thread");
         }
     });
 
@@ -77,23 +81,22 @@ fn main() -> io::Result<()> {
     loop {
         // check for messages from the receiver thread
         if let Ok(message) = rx_rx.try_recv() {
-            chat_history.push(message);
+            chat_history_recv_clone.lock().unwrap().push(message);
             clear_screen();
-            print_chat(&chat_history);
+            print_chat(&chat_history_recv_clone);
             print_prompt();
         }
 
         if let Ok(message) = tx_rx.try_recv() {
-            chat_history.push(message);
+            chat_history_send_clone.lock().unwrap().push(message);
             clear_screen();
-            print_chat(&chat_history);
+            print_chat(&chat_history_send_clone);
             print_prompt();
         }
 
         // sleep for a short duration to avoid busy-waiting
         thread::sleep(std::time::Duration::from_millis(100));
     }
-
 }
 
 fn send_message(socket: &UdpSocket, send_msg: &str) -> io::Result<()> {
@@ -112,13 +115,15 @@ fn listen_for_message(addr: &str, tx: mpsc::Sender<String>) -> io::Result<()> {
     loop {
         let (amt, src) = socket.recv_from(&mut buffer)?;
         let message = format!("{}: {}", src, String::from_utf8_lossy(&buffer[..amt]));
-        tx.send(message).expect("Failed to send message to main thread");
+        tx.send(message)
+            .expect("Failed to send message to main thread");
     }
 }
 
+fn print_chat(chat_history: &Arc<Mutex<Vec<String>>>) {
+    let locked_history = chat_history.lock().unwrap();
 
-fn print_chat(chat_history: &[String]) {
-    for message in chat_history.iter() {
+    for message in locked_history.iter() {
         println!("{}", message);
     }
 }
@@ -138,4 +143,3 @@ fn clear_screen() {
         std::process::Command::new("cls").status().unwrap();
     }
 }
-
